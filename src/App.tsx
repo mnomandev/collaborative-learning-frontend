8566dde2cf0b827cc003783ca76afc1ce4c9fd52
nomandev <nomankhan02432@gmail.com>
2025-02-05 15:18:37 +0500

    project-setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='font-extrabold'>React APP</h1>
+    <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
